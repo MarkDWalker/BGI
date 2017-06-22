1cf1546c8f2e8aa196611de8c7fa5060fc25bedb
Mark Walker <markwalker@Marks-MacBook-Pro.local>
2017-06-22 08:00:05 -0400

    minor changes

---
+++ BeamGrapherForIpadV_0.0.3/CustomCell_WoodFactor.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell_WoodFactor.swift
+//  BeamGrapherForIpadV_0.0.3
+//
+//  Created by Mark Walker on 6/22/17.
+//  Copyright © 2017 Mark Walker. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell_WoodFactor: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
